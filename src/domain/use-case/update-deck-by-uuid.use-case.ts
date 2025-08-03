6bae98f280f370e625ff4949e6c693ddd90b076f
jcarloshg <carlosj12336@gmail.com>
2025-08-02 22:03:38 -0600

    update all

--- src/domain/use-case/update-deck-by-uuid.use-case.ts
+++ src/domain/use-case/update-deck-by-uuid.use-case.ts
@@ -1,4 +1,8 @@
-import { DeckSchema, DeckToUpdateType, DeckType } from "../entities/Deck.entity";
+import {
+  DeckSchema,
+  DeckToUpdateType,
+  DeckType,
+} from "../entities/Deck.entity";
 import { CustomResponse } from "../entities/custom-response.entity";
 import { EntityError } from "../entities/entity-error";
 import { ErrorRepository } from "../repositories/error-repository";
@@ -41,25 +45,34 @@ export class UpdateDeckByUuidUseCase {
    * @param props - The properties for updating the deck.
    * @returns A promise resolving to a CustomResponse.
    */
-  async run(props: UpdateDeckByUuidProps): Promise<CustomResponse<DeckType | null>> {
+  async run(
+    props: UpdateDeckByUuidProps
+  ): Promise<CustomResponse<DeckType | null>> {
     try {
-      
-        // Validate input data
+      // Validate input data
       const { uuid, ...updateData } = props.data;
       const parsed = DeckSchema.partial().safeParse(updateData);
-      if (!parsed.success) {
-        return EntityError.getMessage(parsed.error);
-      }
+      if (!parsed.success) return EntityError.getMessage(parsed.error);
 
       // Update deck
-      const updatedDeck = await this.updateDeckRepository.run(uuid, updateData as DeckToUpdateType);
-      if (!updatedDeck) {
-        return CustomResponse.notFound({ userMessage: "Deck not found", developerMessage: `Deck with uuid ${uuid} not found` });
-      }
-      return CustomResponse.ok(updatedDeck, { userMessage: "Deck updated successfully" });
+      const updatedDeck = await this.updateDeckRepository.run(
+        uuid,
+        updateData as DeckToUpdateType
+      );
+      if (!updatedDeck)
+        return CustomResponse.notFound({
+          userMessage: "Deck not found",
+          developerMessage: `Deck with uuid ${uuid} not found`,
+        });
+
+      return CustomResponse.ok(updatedDeck, {
+        userMessage: "Deck updated successfully",
+      });
+
     } catch (error) {
       if (error instanceof EntityError) return EntityError.getMessage(error);
-      if (error instanceof ErrorRepository) return ErrorRepository.getMessage(error);
+      if (error instanceof ErrorRepository)
+        return ErrorRepository.getMessage(error);
       return CustomResponse.internalServerError();
     }
   }
