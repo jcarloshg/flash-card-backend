304e68a0092b3d43c7151b97bbd05217ec766f2b
jcarloshg <carlosj12336@gmail.com>
2025-08-02 20:35:09 -0600

    create ReadByIdDeckSqliteRepository

--- src/domain/repositories/deck/read-by-id-deck.repository.ts
+++ src/domain/repositories/deck/read-by-id-deck.repository.ts
@@ -1,13 +1,13 @@
 import { ReadByIdRepository } from "../crud-repository/read-by-id.repository";
-import { DeckType } from "../../entities/Deck.entity";
+import { DeckToRepositoryType, DeckType } from "../../entities/Deck.entity";
 
-export class ReadByIdDeckRepository implements ReadByIdRepository<string, DeckType> {
+export class ReadByIdDeckRepository implements ReadByIdRepository<string, DeckToRepositoryType> {
     /**
      * Finds a deck by its unique identifier.
      * @param id - The UUID of the deck.
      * @returns A promise resolving to the deck if found, otherwise null.
      */
-    async findById(id: string): Promise<DeckType | null> {
+    async findById(id: string): Promise<DeckToRepositoryType | null> {
         throw new Error("Method not implemented.");
     }
 }
