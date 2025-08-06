41a77e883449bd00fd428170497b9dd87b3670be
jcarloshg <carlosj12336@gmail.com>
2025-08-06 16:57:11 -0600

    update all

--- src/application/usecases/category/run-update-category-by-uuid.application.ts
+++ src/application/usecases/category/run-update-category-by-uuid.application.ts
@@ -1,8 +1,8 @@
-import { UpdateCategorySqliteRepository } from "@/infrastructure/database/sqlite-02/repositories/category/update-category.sqlite";
 import {
   UpdateCategoryByUuidProps,
   UpdateCategoryByUuidUseCase,
 } from "@/domain/use-case/category/update-category-by-uuid.use-case";
+import { UpdateCategoryPostgresRepository } from "@/infrastructure/database/postgres/repositories/category";
 
 /**
  * Runs the update category by uuid use case in the application layer.
@@ -12,7 +12,7 @@ import {
 export const runUpdateCategoryByUuid = async (
   props: UpdateCategoryByUuidProps
 ) => {
-  const repository = new UpdateCategorySqliteRepository();
+  const repository = new UpdateCategoryPostgresRepository();
   const useCase = new UpdateCategoryByUuidUseCase(repository);
   return await useCase.run(props);
 };
